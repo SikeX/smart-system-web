237eaebb8ce8234b823208f2c0b384f2e191e8f8
xyy <machine.xyy@gmail.com>
2022-03-05 22:40:54 +0800

    评分页面添加评分角色选择,考核任务列表进行权限控制,修改考核内容目录点击判断

--- src/views/smartAssessmentScore/SmartScoreList.vue
+++ src/views/smartAssessmentScore/SmartScoreList.vue
@@ -1,51 +1,72 @@
 <template>
-  <a-card :title='description' :bordered="false">
-    <!-- table区域-begin -->
-    <div>
+  <div>
+    <a-modal v-model="isShowModal" title="请选择参与评分的角色" :maskClosable="false" :closable="false">
+      <template slot="footer">
+        <a-button key="submit" type="primary" @click="handleRoleOk">
+          确 定
+        </a-button>
+      </template>
+      <a-form layout="inline">
+        <a-form-item label="评分角色">
+          <a-select v-model="scoreRole" style="width: 200px">
+            <a-select-option value="team">
+              考核组
+            </a-select-option>
+            <a-select-option value="depart">
+              考核单位
+            </a-select-option>
+          </a-select>
+        </a-form-item>
+      </a-form>
+    </a-modal>
+    <a-card :title='description' :bordered="false">
+      <!-- table区域-begin -->
+      <div>
 
-      <a-table
-        ref="table"
-        size="middle"
-        bordered
-        rowKey="id"
-        class="j-table-force-nowrap"
-        :scroll="{x:true}"
-        :columns="columns"
-        :dataSource="dataSource"
-        :pagination="ipagination"
-        :loading="loading"
-        :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange, type:'radio'}"
-        :customRow="clickThenSelect"
-        @change="handleTableChange">
+        <a-table
+          ref="table"
+          size="middle"
+          bordered
+          rowKey="id"
+          class="j-table-force-nowrap"
+          :scroll="{x:true}"
+          :columns="columns"
+          :dataSource="dataSource"
+          :pagination="ipagination"
+          :loading="loading"
+          :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange, type:'radio'}"
+          :customRow="clickThenSelect"
+          @change="handleTableChange">
 
-        <template slot="htmlSlot" slot-scope="text">
-          <div v-html="text"></div>
-        </template>
-        <template slot="imgSlot" slot-scope="text">
-          <span v-if="!text" style="font-size: 12px;font-style: italic;">无图片</span>
-          <img v-else :src="getImgView(text)" height="25px" alt="" style="max-width:80px;font-size: 12px;font-style: italic;"/>
-        </template>
-        <template slot="fileSlot" slot-scope="text">
-          <span v-if="!text" style="font-size: 12px;font-style: italic;">无文件</span>
-          <a-button
-            v-else
-            :ghost="true"
-            type="primary"
-            icon="download"
-            size="small"
-            @click="downloadFile(text)">
-            下载
-          </a-button>
-        </template>
+          <template slot="htmlSlot" slot-scope="text">
+            <div v-html="text"></div>
+          </template>
+          <template slot="imgSlot" slot-scope="text">
+            <span v-if="!text" style="font-size: 12px;font-style: italic;">无图片</span>
+            <img v-else :src="getImgView(text)" height="25px" alt=""
+                 style="max-width:80px;font-size: 12px;font-style: italic;"/>
+          </template>
+          <template slot="fileSlot" slot-scope="text">
+            <span v-if="!text" style="font-size: 12px;font-style: italic;">无文件</span>
+            <a-button
+              v-else
+              :ghost="true"
+              type="primary"
+              icon="download"
+              size="small"
+              @click="downloadFile(text)">
+              下载
+            </a-button>
+          </template>
 
-        <span slot="action" slot-scope="text, record">
+          <span slot="action" slot-scope="text, record">
           <a @click="handleEdit(record)">编辑</a>
 
-          <a-divider type="vertical" />
+          <a-divider type="vertical"/>
           <a @click="publishMission(record)">发布</a>
-          <a-divider type="vertical" />
+          <a-divider type="vertical"/>
           <a-dropdown>
-            <a class="ant-dropdown-link">更多 <a-icon type="down" /></a>
+            <a class="ant-dropdown-link">更多 <a-icon type="down"/></a>
             <a-menu slot="overlay">
               <a-menu-item>
                 <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.id)">
@@ -56,82 +77,87 @@
           </a-dropdown>
         </span>
 
-      </a-table>
-    </div>
+        </a-table>
+      </div>
 
-    <div>
-      <a-row v-if='selectedMainId'>
-        <smart-answer-page :main-id='selectedMainId'></smart-answer-page>
-      </a-row>
-    </div>
-  </a-card>
+      <div>
+        <a-row v-if='selectedMainId'>
+          <smart-score-page :main-id='selectedMainId'></smart-score-page>
+        </a-row>
+      </div>
+    </a-card>
+  </div>
 </template>
 
 <script>
 
-import { JeecgListMixin } from '@/mixins/JeecgListMixin'
-import { getAction, postAction, putAction } from '@/api/manage'
+import {JeecgListMixin} from '@/mixins/JeecgListMixin'
+import {getAction, postAction, putAction} from '@/api/manage'
 import SmartAssessmentContentList from '@views/smartAssessmentContent/SmartAssessmentContentList'
 import '@/assets/less/TableExpand.less'
-import SmartAnswerPage from '@views/smartAssessmentScore/modules/SmartAnswerPage'
+import SmartScorePage from '@views/smartAssessmentScore/modules/SmartScorePage'
 import SmartScoreInfoModal from '@views/smartAssessmentScore/modules/SmartScoreInfoModal'
 
 export default {
   name: "SmartScoreList",
-  mixins:[JeecgListMixin],
+  mixins: [JeecgListMixin],
   components: {
     SmartScoreInfoModal,
-    SmartAnswerPage,
+    SmartScorePage,
     SmartAssessmentContentList,
   },
-  data () {
+  data() {
     return {
       description: '考核任务列表',
+      disableMixinCreated: true,
+      isShowModal: true,
+      scoreRole: 'team',
       // 表头
       columns: [
         {
-          title:'任务名称',
-          align:"center",
+          title: '任务名称',
+          align: "center",
           dataIndex: 'missionName'
         },
         {
-          title:'考核年份',
-          align:"center",
+          title: '考核年份',
+          align: "center",
           dataIndex: 'assessmentYear'
         },
         {
-          title:'考核时间',
-          align:"center",
+          title: '考核时间',
+          align: "center",
           dataIndex: 'assessmentTime'
         },
         {
-          title:'总分',
-          align:"center",
+          title: '总分',
+          align: "center",
           dataIndex: 'totalPoint'
         },
         {
-          title:'任务状态',
-          align:"center",
+          title: '任务状态',
+          align: "center",
           dataIndex: 'missionStatus'
         },
         {
-          title:'考核要点总数',
-          align:"center",
+          title: '考核要点总数',
+          align: "center",
           dataIndex: 'keyPointsAmount'
         }
       ],
       url: {
-        list: "/smartAssessmentMission/smartAssessmentMission/list",
+        list: "/smartAssessmentMission/smartAssessmentMission/teamMissionList",
+        departmentMissionList: "/smartAssessmentMission/smartAssessmentMission/departmentMissionList",
+        teamMissionList: "/smartAssessmentMission/smartAssessmentMission/teamMissionList",
         delete: "/smartAssessmentMission/smartAssessmentMission/delete",
         publish: "/smartAssessmentMission/smartAssessmentMission/publish",
         deleteBatch: "/smartAssessmentMission/smartAssessmentMission/deleteBatch",
         exportXlsUrl: "/smartAssessmentMission/smartAssessmentMission/exportXls",
         importExcelUrl: "smartAssessmentMission/smartAssessmentMission/importExcel",
       },
-      dictOptions:{
-      },
+      dictOptions: {},
       /* 分页参数 */
-      ipagination:{
+      ipagination: {
         current: 1,
         pageSize: 5,
         pageSizeOptions: ['5', '10', '50'],
@@ -142,20 +168,29 @@ export default {
         showSizeChanger: true,
         total: 0
       },
-      selectedMainId:'',
-      superFieldList:[],
+      selectedMainId: '',
+      superFieldList: [],
     }
   },
   created() {
     this.getSuperFieldList();
   },
   computed: {
-    importExcelUrl: function(){
+    importExcelUrl: function () {
       return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`;
     }
   },
   methods: {
-    initDictConfig(){
+    handleRoleOk() {
+      if (this.scoreRole === "depart") {
+        this.url.list = this.url.departmentMissionList;
+      } else {
+        this.url.list = this.url.teamMissionList;
+      }
+      this.loadData(1)
+      this.isShowModal = false;
+    },
+    initDictConfig() {
     },
     clickThenSelect(record) {
       return {
@@ -169,15 +204,15 @@ export default {
     onClearSelected() {
       this.selectedRowKeys = [];
       this.selectionRows = [];
-      this.selectedMainId=''
+      this.selectedMainId = ''
     },
     onSelectChange(selectedRowKeys, selectionRows) {
-      this.selectedMainId=selectedRowKeys[0]
+      this.selectedMainId = selectedRowKeys[0]
       this.selectedRowKeys = selectedRowKeys;
       this.selectionRows = selectionRows;
     },
     loadData(arg) {
-      if(!this.url.list){
+      if (!this.url.list) {
         this.$message.error("请设置url.list属性!")
         return
       }
@@ -193,20 +228,20 @@ export default {
           this.dataSource = res.result.records;
           this.ipagination.total = res.result.total;
         }
-        if(res.code===510){
+        if (res.code === 510) {
           this.$message.warning(res.message)
         }
         this.loading = false;
       })
     },
-    getSuperFieldList(){
-      let fieldList=[];
-      fieldList.push({type:'string',value:'missionName',text:'任务名称',dictCode:''})
-      fieldList.push({type:'string',value:'assessmentYear',text:'考核年份',dictCode:''})
-      fieldList.push({type:'datetime',value:'assessmentTime',text:'考核时间'})
-      fieldList.push({type:'int',value:'totalPoint',text:'总分',dictCode:''})
-      fieldList.push({type:'string',value:'missionStatus',text:'任务状态',dictCode:''})
-      fieldList.push({type:'int',value:'keyPointsAmount',text:'考核要点总数',dictCode:''})
+    getSuperFieldList() {
+      let fieldList = [];
+      fieldList.push({type: 'string', value: 'missionName', text: '任务名称', dictCode: ''})
+      fieldList.push({type: 'string', value: 'assessmentYear', text: '考核年份', dictCode: ''})
+      fieldList.push({type: 'datetime', value: 'assessmentTime', text: '考核时间'})
+      fieldList.push({type: 'int', value: 'totalPoint', text: '总分', dictCode: ''})
+      fieldList.push({type: 'string', value: 'missionStatus', text: '任务状态', dictCode: ''})
+      fieldList.push({type: 'int', value: 'keyPointsAmount', text: '考核要点总数', dictCode: ''})
       this.superFieldList = fieldList
     },
     publishMission(record) {
