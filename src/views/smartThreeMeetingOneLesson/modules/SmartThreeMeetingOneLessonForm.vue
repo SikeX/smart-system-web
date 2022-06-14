3d93374ddf54a163ba8070e728e0c07b7cf16a28
李召云 <33644961+lizhaoyun@users.noreply.github.com>
2022-06-14 15:02:45 +0800

    三会一课审核 (#199)
    * 三会一课
    
    * 三会一课导出权限控制及添加导出日志
    
    * 三会一课添加高拍仪
    
    * 三会一课修改高拍仪代码
    
    * 三会一课修改高拍仪代码
    
    * 高拍仪
    
    * 三会一课修改
    
    * 三会一课审核

--- src/views/smartThreeMeetingOneLesson/modules/SmartThreeMeetingOneLessonForm.vue
+++ src/views/smartThreeMeetingOneLesson/modules/SmartThreeMeetingOneLessonForm.vue
@@ -75,7 +75,7 @@
               <j-date placeholder="请选择创建日期" v-model="model.createTime" :show-time="true" date-format="YYYY-MM-DD HH:mm:ss" style="width: 100%" />
             </a-form-model-item>
           </a-col> -->
-
+          
           <!-- <a-col :span="24" >
             <a-form-model-item label="审核状态" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="verifyStatus">
               <a-input v-model="model.verifyStatus" placeholder="请输入审核状态" ></a-input>
@@ -83,13 +83,13 @@
           </a-col> -->
           <a-col :span="24" >
             <a-form-model-item label="附件" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="files">
-              <j-upload v-model="model.files"></j-upload>
               <a-button icon="camera" @click="eloamScan">高拍仪拍照</a-button>
+              <eloam-modal ref="modalForm" @ok="scanOk"></eloam-modal>
+              <j-upload v-model="model.files"></j-upload>
             </a-form-model-item>
           </a-col>
         </a-row>
       </a-form-model>
-      <eloam-modal ref="modalForm" @ok='scanOk' biz-path='eloam'></eloam-modal>
     </j-form-container>
     <!-- 子表单区域 -->
     <a-tabs v-model="activeKey" @change="handleChangeTabs">
@@ -118,83 +118,138 @@
           :actionButton="true"/>
       </a-tab-pane>
     </a-tabs>
+    <!-- <eloam-modal ref="modalForm" @ok='scanOk'></eloam-modal> -->
+    <!-- <eloam-modal ref="modalForm" @ok="scanOk" biz-path="eloam"></eloam-modal> -->
   </a-spin>
 </template>
 
 <script>
 
-import { getAction } from '@/api/manage'
-import { FormTypes,getRefPromise,VALIDATE_NO_PASSED } from '@/utils/JEditableTableUtil'
-import { JEditableTableModelMixin } from '@/mixins/JEditableTableModelMixin'
-import { validateDuplicateValue } from '@/utils/util'
-import SelectUserByDep from '@/components/jeecgbiz/modal/SelectUserByDep'
-import EloamModal from '@views/eloam/modules/EloamModal'
+  import { getAction } from '@/api/manage'
+  import { FormTypes,getRefPromise,VALIDATE_NO_PASSED } from '@/utils/JEditableTableUtil'
+  import { JEditableTableModelMixin } from '@/mixins/JEditableTableModelMixin'
+  import { validateDuplicateValue } from '@/utils/util'
+  import SelectUserByDep from '@/components/jeecgbiz/modal/SelectUserByDep'
+  import EloamModal from '@views/eloam/modules/EloamModal'
 
-export default {
-  name: 'SmartThreeMeetingOneLessonForm',
-  mixins: [JEditableTableModelMixin],
-  components: {
-    SelectUserByDep,EloamModal
-  },
-  data() {
-    return {
-      labelCol: {
-        xs: { span: 24 },
-        sm: { span: 6 },
-      },
-      wrapperCol: {
-        xs: { span: 24 },
-        sm: { span: 16 },
-      },
-      labelCol2: {
-        xs: { span: 24 },
-        sm: { span: 3 },
-      },
-      wrapperCol2: {
-        xs: { span: 24 },
-        sm: { span: 20 },
-      },
-      model:{
-        hostId:'',
-        hostName:'',
-        recorderId:'',
-        recorderName:'',
-      },
-      // 新增时子表默认添加几行空数据
-      addDefaultRowNum: 1,
-      validatorRules: {
-        time: [
-          { required: true, message: '请输入时间!'},
-        ],
-        type: [
-          { required: true, message: '请输入类型!'},
-        ],
-        theme: [
-          { required: true, message: '请输入主题!'},
-        ],
-        createTime: [
-          { required: true, message: '请输入创建日期!'},
-        ],
-      },
-      refKeys: ['smartThreeMeetingOneLessonParticipants', 'smartThreeMeetingOneLessonAnnex', ],
-      tableKeys:['smartThreeMeetingOneLessonParticipants', 'smartThreeMeetingOneLessonAnnex', ],
-      activeKey: 'smartThreeMeetingOneLessonParticipants',
-      // 三会一课参会人员表
-      smartThreeMeetingOneLessonParticipantsTable: {
-        loading: false,
-        dataSource: [],
-        columns: [
-          {
-            title: '参会人员工号',
-            key: 'participantsNumber',
-            type: FormTypes.input,
-            width:"200px",
-            placeholder: '请输入${title}',
-            defaultValue:'',
-            validateRules: [{ required: true, message: '${title}不能为空' }],
-          },
-        ]
-      },
+  export default {
+    name: 'SmartThreeMeetingOneLessonForm',
+    mixins: [JEditableTableModelMixin],
+    components: {
+      SelectUserByDep,EloamModal
+    },
+    data() {
+      return {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 6 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+        labelCol2: {
+          xs: { span: 24 },
+          sm: { span: 3 },
+        },
+        wrapperCol2: {
+          xs: { span: 24 },
+          sm: { span: 20 },
+        },
+        model:{
+          hostId:'',
+          hostName:'',
+          recorderId:'',
+          recorderName:'',
+        },
+        // 新增时子表默认添加几行空数据
+        addDefaultRowNum: 1,
+        validatorRules: {
+           time: [
+              { required: true, message: '请输入时间!'},
+           ],
+           type: [
+              { required: true, message: '请输入类型!'},
+           ],
+           theme: [
+              { required: true, message: '请输入主题!'},
+           ],
+           createTime: [
+              { required: true, message: '请输入创建日期!'},
+           ],
+        },
+        refKeys: ['smartThreeMeetingOneLessonParticipants', 'smartThreeMeetingOneLessonAnnex', ],
+        tableKeys:['smartThreeMeetingOneLessonParticipants', 'smartThreeMeetingOneLessonAnnex', ],
+        activeKey: 'smartThreeMeetingOneLessonParticipants',
+        // 三会一课参会人员表
+        smartThreeMeetingOneLessonParticipantsTable: {
+          loading: false,
+          dataSource: [],
+          columns: [
+            {
+              title: '参会人员工号',
+              key: 'participantsNumber',
+              type: FormTypes.input,
+              width:"200px",
+              placeholder: '请输入${title}',
+              defaultValue:'',
+              validateRules: [{ required: true, message: '${title}不能为空' }],
+            },
+          ]
+        },
+      //   // 三会一课附件表
+      //   smartThreeMeetingOneLessonAnnexTable: {
+      //     loading: false,
+      //     dataSource: [],
+      //     columns: [
+      //       {
+      //         title: '附件说明',
+      //         key: 'fileDescription',
+      //         type: FormTypes.input,
+      //         width:"200px",
+      //         placeholder: '请输入${title}',
+      //         defaultValue:'',
+      //       },
+      //       {
+      //         title: '附件文件路径',
+      //         key: 'fileLocation',
+      //         type: FormTypes.file,
+      //         token:true,
+      //         responseName:"message",
+      //         width:"200px",
+      //         placeholder: '请选择文件',
+      //         defaultValue:'',
+      //         validateRules: [{ required: true, message: '${title}不能为空' }],
+      //       },
+      //       {
+      //         title: '上传时间',
+      //         key: 'uploadTime',
+      //         type: FormTypes.date,
+      //         width:"200px",
+      //         placeholder: '请输入${title}',
+      //         defaultValue:'',
+      //         validateRules: [{ required: true, message: '${title}不能为空' }],
+      //       },
+      //       {
+      //         title: '下载次数',
+      //         key: 'downloadTimes',
+      //         type: FormTypes.inputNumber,
+      //         disabled:true,
+      //         width:"200px",
+      //         placeholder: '请输入${title}',
+      //         defaultValue:'',
+      //       },
+      //     ]
+      //   },
+      //   url: {
+      //     add: "/smartThreeMeetingOneLesson/smartThreeMeetingOneLesson/add",
+      //     edit: "/smartThreeMeetingOneLesson/smartThreeMeetingOneLesson/edit",
+      //     queryById: "/smartThreeMeetingOneLesson/smartThreeMeetingOneLesson/queryById",
+      //     smartThreeMeetingOneLessonParticipants: {
+      //       list: '/smartThreeMeetingOneLesson/smartThreeMeetingOneLesson/querySmartThreeMeetingOneLessonParticipantsByMainId'
+      //     },
+      //   ]
+      // },
       // 三会一课附件表
       smartThreeMeetingOneLessonAnnexTable: {
         loading: false,
@@ -290,32 +345,34 @@ export default {
       let values = this.tableKeys.map(key => getRefPromise(this, key))
       return Promise.all(values)
     },
-    /** 调用完edit()方法之后会自动调用此方法 */
-    editAfter() {
-      this.$nextTick(() => {
-      })
-      // 加载子表数据
-      if (this.model.id) {
-        let params = { id: this.model.id }
-        this.requestSubTableData(this.url.smartThreeMeetingOneLessonParticipants.list, params, this.smartThreeMeetingOneLessonParticipantsTable)
-        this.requestSubTableData(this.url.smartThreeMeetingOneLessonAnnex.list, params, this.smartThreeMeetingOneLessonAnnexTable)
-      }
-    },
-    //校验所有一对一子表表单
-    validateSubForm(allValues){
-      return new Promise((resolve,reject)=>{
-        Promise.all([
-        ]).then(() => {
-          resolve(allValues)
-        }).catch(e => {
-          if (e.error === VALIDATE_NO_PASSED) {
-            // 如果有未通过表单验证的子表，就自动跳转到它所在的tab
-            this.activeKey = e.index == null ? this.activeKey : this.refKeys[e.index]
-          } else {
-            console.error(e)
-          }
+    // methods: {
+    //   getHostUser(back){
+    //     let that = this
+    //     console.log(back);
+    //     that.model.hostId = back[0].id
+    //     that.model.hostName = back[0].realname
+    //   },
+
+    //   getRecorderUser(back){
+    //     let that = this
+    //     console.log(back);
+    //     that.model.recorderId = back[0].id
+    //     that.model.recorderName = back[0].realname
+    //   },
+
+    //   addBefore(){
+    //     this.smartThreeMeetingOneLessonParticipantsTable.dataSource=[]
+    //     this.smartThreeMeetingOneLessonAnnexTable.dataSource=[]
+    //   },
+    //   getAllTable() {
+    //     let values = this.tableKeys.map(key => getRefPromise(this, key))
+    //     return Promise.all(values)
+    //   },
+      /** 调用完edit()方法之后会自动调用此方法 */
+      editAfter() {
+        this.$nextTick(() => {
         })
-      })
+      // })
     },
     /** 整理成formData */
     classifyIntoFormData(allValues) {
@@ -333,19 +390,39 @@ export default {
       this.$refs.modalForm.open()
     },
     scanOk(url) {
-      let image = url
-      let filed = 'files'
-      if (image) {
-        let arr = []
-        // 考虑如果存在已经上传的文件，则拼接起来，没有则直接添加
-        if (this.model[filed]) {
-          arr.push(this.model[filed])
+        let image = url
+        // 请根据自己表单中的字段名称修改 field 变量的值
+        let field = 'files'
+        if (image) {
+            let arr = []
+            // 考虑如果存在已经上传的文件，则拼接起来，没有则直接添加
+            if (this.model[field]) {
+              arr.push(this.model[field])
+            }
+            arr.push(image)
+            this.$set(this.model, field, arr.join())
         }
-        arr.push(image)
-        // 更新表单中文件url字段, files 为字段名称
-        this.$set(this.model, filed, arr.join())
-      }
     },
+      // validateError(msg){
+      //   this.$message.error(msg)
+      // },
+      // eloamScan() {
+      //   this.$refs.modalForm.open()
+      // },
+      // scanOk(url) {
+      //   let image = url
+      //   // 请根据自己表单中的字段名称修改 field 变量的值
+      //   let field = 'files'
+      //   if (image) {
+      //       let arr = []
+      //       // 考虑如果存在已经上传的文件，则拼接起来，没有则直接添加
+      //       if (this.model[field]) {
+      //         arr.push(this.model[field])
+      //       }
+      //       arr.push(image)
+      //       this.$set(this.model, field, arr.join())
+      //   }
+      // },
 
   }
 }
