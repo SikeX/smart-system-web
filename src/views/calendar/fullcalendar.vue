e624b88b6a371ae320cd9526639cab8b3eeaa7d3
XYY <machine.xyy@gmail.com>
2022-06-27 22:03:34 +0800

    feat: 工作日历修改

--- src/views/calendar/fullcalendar.vue
+++ src/views/calendar/fullcalendar.vue
@@ -3,17 +3,17 @@
     <a-row style='margin: 10px 0px'>
       <a-col>
         <a-button-group style='margin-top: 10px'>
-          <a-button @click='calendarApi.prevYear()'>
+          <a-button @click='prevYear'>
             <a-icon type='double-left' />
           </a-button>
-          <a-button @click='calendarApi.prev()'>
+          <a-button @click='prev'>
             <a-icon type='left' />
           </a-button>
-          <a-button @click='calendarApi.today()'> Today</a-button>
-          <a-button @click='calendarApi.next()'>
+          <a-button @click='today'> 今天</a-button>
+          <a-button @click='next'>
             <a-icon type='right' />
           </a-button>
-          <a-button @click='calendarApi.nextYear()'>
+          <a-button @click='nextYear'>
             <a-icon type='double-right' />
           </a-button>
         </a-button-group>
@@ -24,6 +24,7 @@
           </a-select-option>
         </a-select>
         <a-button @click='handleAdd' type='primary' icon='plus' style='margin-top: 10px;margin-left: 10px;'>新增</a-button>
+        <a-button type='primary' @click='loadData' icon='reload' style='margin-left: 8px'>刷新</a-button>
       </a-col>
     </a-row>
 
@@ -56,7 +57,7 @@ import { getAction, httpAction } from '../../api/manage'
 
 export default {
   name: 'fullcalendar',
-  // mixins: [JeecgListMixin, mixinDevice],
+  mixins: [JeecgListMixin, mixinDevice],
   components: {
     CalendarEventModal,
     FullCalendar // make the <FullCalendar> tag available
@@ -130,12 +131,23 @@ export default {
         selectMirror: true,
         dayMaxEvents: true,
         weekends: true,
+        fixedWeekCount: false,
+        showNonCurrentDates: false,
+        // slotEventOverlap: false,
         select: this.handleDateSelect,
         eventClick: this.handleEventClick,
         eventDrop: this.handleEventDrop,
         eventResize: this.handleEventDrop,
         eventsSet: this.handleEvents,
+        slotLabelFormat:{
+          hour: 'numeric',
+          minute: '2-digit',
+          omitZeroMinute: false,
+          meridiem: 'short'
+        },
+        locale: 'zh-cn',
         timeZone: 'local',
+        allDayContent: '整天',
         firstDay: this.firstDay,
         height: this.calendarHeight,
         views: {
@@ -170,7 +182,58 @@ export default {
       currentEvents: [],
       calendarApi: null,
       loading: false,
+      disableMixinCreated: true,
 
+      columns: [
+        {
+          title: '#',
+          dataIndex: '',
+          key:'rowIndex',
+          width:60,
+          align:"center",
+          customRender:function (t,r,index) {
+            return parseInt(index)+1;
+          }
+        },
+        {
+          title:'创建人',
+          align:"center",
+          dataIndex: 'createBy'
+        },
+        {
+          title:'事件标题',
+          align:"center",
+          dataIndex: 'title'
+        },
+        {
+          title:'事件详情',
+          align:"center",
+          dataIndex: 'eventDetail'
+        },
+        {
+          title:'完成情况',
+          align:"center",
+          dataIndex: 'isFinished'
+        },
+        {
+          title:'开始时间',
+          align:"center",
+          dataIndex: 'startTime'
+        },
+        {
+          title:'结束时间',
+          align:"center",
+          dataIndex: 'endTime'
+        },
+        {
+          title: '操作',
+          dataIndex: 'action',
+          align:"center",
+          fixed:"right",
+          width:147,
+          scopedSlots: { customRender: 'action' }
+        }
+      ],
       url: {
         list: '/calendarEvent/calendarEvent/list',
         edit: '/calendarEvent/calendarEvent/edit',
@@ -199,12 +262,13 @@ export default {
         value: 'listDay'
       }],
       dictOptions: {},
-      superFieldList: []
+      superFieldList: [],
+
     }
   },
   mounted() {
     this.calendarApi = this.$refs.fullCalendar.getApi()
-    this.clearEvents()
+    this.setParams()
     this.loadData()
   },
   methods: {
@@ -212,9 +276,59 @@ export default {
       // 可以调用此函数重新绘制
       this.calendarApi.render()
     },
+    setParams() {
+      // 根据不同的视图，设置查询条件事件
+      let date = this.calendarApi.getDate()
+      let moment = this.$moment(date)
+      let type = this.calendarApi.view.type
+      this.queryParam.today = moment.format('YYYY-MM-DD HH:mm:ss')
+      console.log(date)
+      let that = this
+      if (type == 'timeGridDay' || type == 'listDay') {
+        // 当前日事件
+        that.queryParam.startTime_b = moment.startOf('day').format('YYYY-MM-DD HH:mm:ss')
+        that.queryParam.startTime_e = moment.endOf('day').format('YYYY-MM-DD HH:mm:ss')
+      } else if (type == 'timeGridWeek' || type == 'listWeek') {
+        // 当前周事件
+        that.queryParam.startTime_b = moment.startOf('week').format('YYYY-MM-DD HH:mm:ss')
+        that.queryParam.startTime_e = moment.endOf('week').format('YYYY-MM-DD HH:mm:ss')
+      } else if (type == 'dayGridMonth' || type == 'listMonth') {
+        // 当前月事件
+        that.queryParam.startTime_b = moment.startOf('month').format('YYYY-MM-DD HH:mm:ss')
+        that.queryParam.startTime_e = moment.endOf('month').format('YYYY-MM-DD HH:mm:ss')
+      }
+    },
+    today() {
+      // 当前日事件
+      this.calendarApi.today()
+      this.setParams()
+      this.loadData()
+    },
+    prev() {
+      this.calendarApi.prev()
+      this.setParams()
+      this.loadData()
+    },
+    prevYear() {
+      this.calendarApi.prevYear()
+      this.setParams()
+      this.loadData()
+    },
+    nextYear() {
+      this.calendarApi.nextYear()
+      this.setParams()
+      this.loadData()
+    },
+    next() {
+      this.calendarApi.next()
+      this.setParams()
+      this.loadData()
+    },
 
     changeView(type) {
       this.calendarApi.changeView(type)
+      this.setParams(type)
+      this.loadData()
     },
 
     handleWeekendsToggle() {
@@ -222,12 +336,12 @@ export default {
     },
 
     modalFormOk() {
-      this.clearEvents()
       this.loadData()
     },
 
     handleDateSelect(selectInfo) {
       this.$refs.modalForm.add(selectInfo)
+      console.log(selectInfo)
       this.$refs.modalForm.title = '新增'
       this.$refs.modalForm.disableSubmit = false
     },
@@ -289,24 +403,25 @@ export default {
       this.$refs.modalForm.title = '新增'
       this.$refs.modalForm.disableSubmit = false
     },
-
     loadData(arg) {
-      if (!this.url.list) {
-        this.$message.error('请设置url.list属性!')
+      this.clearEvents()
+
+      if(!this.url.list){
+        this.$message.error("请设置url.list属性!")
         return
       }
-      this.loading = true
       //加载数据 若传入参数1则加载第一页的内容
       if (arg === 1) {
-        this.ipagination.current = 1
+        this.ipagination.current = 1;
       }
-      let params = {}//查询条件
-      this.loading = true
+      var params = this.getQueryParams();//查询条件
+      this.loading = true;
       getAction(this.url.list, params).then((res) => {
         if (res.success) {
-          for (let i = 0; i < res.result.records.length; i++) {
-            let event = res.result.records[i]
-            // 向日历中添加事件
+          let type = this.calendarApi.view.type
+          for (let i = 0; i < res.result.length; i++) {
+            let event = res.result[i]
+
             this.calendarApi.addEvent({
               id: event.id,
               title: event.title,
@@ -323,7 +438,7 @@ export default {
       }).finally(() => {
         this.loading = false
       })
-    }
+    },
   }
 }
 </script>
