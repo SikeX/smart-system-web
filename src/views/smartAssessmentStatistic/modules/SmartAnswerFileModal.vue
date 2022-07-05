8f3b37536e5a9c3f19d20c5f930427cdf60d98f7
XYY <machine.xyy@gmail.com>
2022-07-05 13:59:18 +0800

    feat: 考核内容目录修改,增加提示信息

---
+++ src/views/smartAssessmentStatistic/modules/SmartAnswerFileModal.vue
@@ -0,0 +1,167 @@
+<template>
+  <j-modal
+    :title='title'
+    :width='width'
+    :visible='visible'
+    :confirmLoading='confirmLoading'
+    switchFullscreen
+    @ok='handleOk'
+    :okButtonProps="{ class:{'jee-hidden': disableSubmit} }"
+    @cancel='handleCancel'
+    cancelText='关闭'>
+    <a-spin :spinning='confirmLoading'>
+      <a-form-model ref='form' :model='model' :rules='validatorRules'>
+        <a-row>
+          <a-col :span='24'>
+            <a-form-model-item label='附件说明' :labelCol='labelCol' :wrapperCol='wrapperCol'
+                               prop='attachmentDescription'>
+              <a-textarea v-model='model.attachmentDescription' rows='4' placeholder='请输入附件说明' :disabled='disableSubmit' />
+            </a-form-model-item>
+          </a-col>
+          <a-col :span='24'>
+            <a-form-model-item label='工作开始时间' :labelCol='labelCol' :wrapperCol='wrapperCol' prop='workTime'>
+              <j-date placeholder='请选择工作开始时间' v-model='model.workTime' :show-time='true'
+                      date-format='YYYY-MM-DD HH:mm:ss' style='width: 100%' :disabled='disableSubmit' />
+            </a-form-model-item>
+          </a-col>
+          <a-col :span='24'>
+            <a-form-model-item label='附件' :labelCol='labelCol' :wrapperCol='wrapperCol' prop='attachment'>
+              <a-button v-if="!disableSubmit" icon="camera" @click="eloamScan">高拍仪拍照</a-button>
+              <eloam-modal ref="modalForm" @ok='scanOk'></eloam-modal>
+              <j-upload v-model='model.attachment' :disabled='disableSubmit' :buttonVisible="!disableSubmit"></j-upload>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span='24'>
+            <a-form-model-item label='上报人' :labelCol='labelCol' :wrapperCol='wrapperCol' prop='uploadUser'>
+              <j-select-user-by-dep v-model='model.uploadUser' placeholder='请输入上报人' disabled></j-select-user-by-dep>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span='24'>
+            <a-form-model-item label='上传时间' :labelCol='labelCol' :wrapperCol='wrapperCol' prop='createTime'>
+              <j-date placeholder='请选择上传时间' v-model='model.createTime' :show-time='true'
+                      date-format='YYYY-MM-DD HH:mm:ss' style='width: 100%' disabled />
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+      </a-form-model>
+    </a-spin>
+  </j-modal>
+</template>
+
+<script>
+
+import { httpAction } from '@/api/manage'
+import { validateDuplicateValue } from '@/utils/util'
+import EloamModal from "../../eloam/modules/EloamModal";
+
+export default {
+  name: 'SmartAnswerFileModal',
+  components: {
+    EloamModal
+  },
+  props: {
+    mainId: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      title: '操作',
+      width: 800,
+      visible: false,
+      model: {},
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 5 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 }
+      },
+
+      confirmLoading: false,
+      disableSubmit: false,
+      validatorRules: {},
+      url: {
+        add: '/smartAnswerAssContent/smartAnswerAssContent/addSmartAnswerFile',
+        edit: '/smartAnswerAssContent/smartAnswerAssContent/editSmartAnswerFile'
+      }
+
+    }
+  },
+  created() {
+    //备份model原始值
+    this.modelDefault = JSON.parse(JSON.stringify(this.model))
+  },
+  methods: {
+    eloamScan() {
+      this.$refs.modalForm.open()
+    },
+    scanOk(url) {
+      let image = url
+      let dic_name = 'attachment'
+      if (image) {
+        let arr = []
+        // 考虑如果存在已经上传的文件，则拼接起来，没有则直接添加
+        if (this.model[dic_name]) {
+          arr.push(this.model[dic_name])
+        }
+        arr.push(image)
+        // 更新表单中文件url字段, files 为字段名称
+        this.$set(this.model, dic_name, arr.join())
+      }
+    },
+    add() {
+      this.edit(this.modelDefault)
+    },
+    edit(record) {
+      this.model = Object.assign({}, record)
+      this.visible = true
+    },
+    close() {
+      this.$emit('close')
+      this.visible = false
+      this.$refs.form.clearValidate()
+    },
+    handleOk() {
+      const that = this
+      // 触发表单验证
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          that.confirmLoading = true
+          let httpurl = ''
+          let method = ''
+          if (!this.model.id) {
+            httpurl += this.url.add
+            method = 'post'
+          } else {
+            httpurl += this.url.edit
+            method = 'put'
+          }
+          this.model['mainId'] = this.mainId
+          httpAction(httpurl, this.model, method).then((res) => {
+            if (res.success) {
+              that.$message.success(res.message)
+              that.$emit('ok')
+            } else {
+              that.$message.warning(res.message)
+            }
+          }).finally(() => {
+            that.confirmLoading = false
+            that.close()
+          })
+        } else {
+          return false
+        }
+      })
+    },
+    handleCancel() {
+      this.close()
+    }
+
+
+  }
+}
+</script>
--- src/views/smartAssessmentStatistic/modules/SubMenu.vue
+++ src/views/smartAssessmentStatistic/modules/SubMenu.vue
@@ -1,19 +1,30 @@
 <template>
   <a-sub-menu :key="menuInfo.assContentId" v-bind="$props" v-on="$listeners" >
         <span slot="title">
-          <span>{{ menuInfo.assContentId_dictText }}</span>
+          <a-tooltip placement="topLeft">
+            <template #title>{{ menuInfo.assContentId_dictText }}</template>
+            <span>{{ menuInfo.assContentId_dictText }}</span>
+          </a-tooltip>
         </span>
     <template v-for="item in menuInfo.children">
       <a-menu-item v-if="item.isKey === 1" :key="item.id + ',' + item.assContentId">
         <div v-if='item.uploadCount'>
-          <a-icon type='check-square' theme='twoTone' />
-          <span>{{ item.assContentId_dictText }}</span>
-          <el-badge :value="item.uploadCount" :max="99" type="primary"/>
+<!--          <el-badge :value="item.uploadCount" :max="99" type="primary"/>-->
+          <a-badge title="此要点上传数目" :count="item.uploadCount" :number-style="{ backgroundColor: '#52c41a' }" />
+          <!--                  <a-icon type='check-square' theme='twoTone' />-->
+          <a-tooltip placement="topLeft">
+            <template #title>{{ item.assContentId_dictText }}</template>
+            <span>{{ item.assContentId_dictText }}</span>
+          </a-tooltip>
         </div>
         <div v-else>
-          <a-icon type="edit" theme="twoTone" />
-          <span>{{ item.assContentId_dictText }}</span>
-          <el-badge :value="item.uploadCount" :max="99" />
+<!--          <el-badge :value="item.uploadCount" :max="99" />-->
+          <a-badge title="此要点暂未上传任何内容" show-zero :count="item.uploadCount" />
+          <!--                  <a-icon type="edit" theme="twoTone" />-->
+          <a-tooltip placement="topLeft">
+            <template #title>{{ item.assContentId_dictText }}</template>
+            <span>{{ item.assContentId_dictText }}</span>
+          </a-tooltip>
         </div>
       </a-menu-item>
       <sub-menu v-else :key="item.id + ',' + item.assContentId" :menu-info="item"/>
