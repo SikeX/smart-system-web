a702333a58830ede494adb861f0d9e81a8019e5e
XYY <machine.xyy@gmail.com>
2022-07-31 22:04:45 +0800

    feat: 考核组和考核单位取消导入按钮

--- src/views/smartAssessmentTeam/SmartAssessmentTeamList.vue
+++ src/views/smartAssessmentTeam/SmartAssessmentTeamList.vue
@@ -29,9 +29,9 @@
       <div class="table-operator">
         <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>
         <a-button type="primary" icon="download" @click="handleExportXls('考核组')">导出</a-button>
-        <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" accept='.xls,.xlsx' :action="importExcelUrl" @change="handleImportExcel">
-          <a-button type="primary" icon="import">导入</a-button>
-        </a-upload>
+<!--        <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" accept='.xls,.xlsx' :action="importExcelUrl" @change="handleImportExcel">-->
+<!--          <a-button type="primary" icon="import">导入</a-button>-->
+<!--        </a-upload>-->
         <!-- 高级查询区域 -->
         <j-super-query :fieldList="superFieldList" ref="superQueryModal" @handleSuperQuery="handleSuperQuery"></j-super-query>
         <a-dropdown v-if="selectedRowKeys.length > 0">
