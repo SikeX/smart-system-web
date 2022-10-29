85bad86be9cb835541338cfc08e37445692349cf
XYY <machien.xyy@gmail.com>
2022-10-29 16:34:17 +0800

    feat: 民主生活会和组织生活会详情页面高拍仪按钮隐藏

--- src/views/smartDemocraticLifeMeeting/modules/SmartDemocraticLifeMeetingForm.vue
+++ src/views/smartDemocraticLifeMeeting/modules/SmartDemocraticLifeMeetingForm.vue
@@ -61,7 +61,7 @@
           </a-col>
           <a-col :span="24" >
             <a-form-model-item label="附件" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="files">
-              <a-button icon="camera" @click="eloamScan">高拍仪拍照</a-button>
+              <a-button v-if='!disabled' icon="camera" @click="eloamScan">高拍仪拍照</a-button>
               <eloam-modal ref="modalForm" @ok='scanOk'></eloam-modal>
               <j-upload v-model="model.path" ></j-upload>
             </a-form-model-item>
